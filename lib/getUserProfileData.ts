05a18a2e948de4e097c96823050b847207e5dd3e
ad <stepanian.adam@live.com>
2014-10-30 14:13:09 -0400

    Modified getUserProfileData.ts according to CR

--- lib/getUserProfileData.ts
+++ lib/getUserProfileData.ts
@@ -1,22 +1,29 @@
 import express = require("express");
-
 import UserProfile = require("../classes/UserProfile");
+import getLogger = require("mykoop-logger");
+var logger = getLogger(module);
+
 function getUserProfileData(db: mkdatabase.Module, req: express.Request, res: express.Response){
   var profil;
   var id = req.param("id", null);
-  if (!id) {
+  if (id === null) {
+    logger.debug("Invalid parameter for request getUserProfileData");
     res.end(400);
     return;
   }
-//testing
   if (db) {
     db.getConnection(function(err,connection) {
       var query = connection.query(
         "SELECT ?? FROM user WHERE id = ?",
         [UserProfile.COLUMNS,id],
         function (err,rows){
-          if (err || rows.length !== 1) {
+          if (err) {
             res.status(500).end({ error: err.toString() });
+            return;
+          }
+          if(rows.length !== 1){
+            logger.debug("Invalid user Id provided to getUserProfileData");
+            res.end(500);
             return;
           }
           var userRow = rows[0];
