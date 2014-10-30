798a87f5de866e0e7d2480ac76b31321471126d1
ad <stepanian.adam@live.com>
2014-10-30 16:20:26 -0400

    Changed UserProfile class to match new register form

--- classes/UserProfile.js
+++ classes/UserProfile.js
@@ -1,8 +1,18 @@
 var UserProfile = (function () {
     function UserProfile(userRow) {
-        this.email = userRow.email, this.firstname = userRow.firstname, this.lastname = userRow.lastname, this.birthdate = userRow.birthdate, this.phone = userRow.phone, this.origin = userRow.origin;
+        this.email = userRow.email, this.firstname = userRow.firstname, this.lastname = userRow.lastname, this.birthdate = userRow.birthdate, this.phone = userRow.phone, this.origin = userRow.origin, this.referral = userRow.referral, this.usageFrequency = userRow.usageFrequency, this.usageNote = userRow.usageNote;
     }
-    UserProfile.COLUMNS = ["email", "firstname", "lastname", "birthdate", "phone", "origin"];
+    UserProfile.COLUMNS = [
+        "email",
+        "firstname",
+        "lastname",
+        "birthdate",
+        "phone",
+        "origin",
+        "referral",
+        "usageFrequency",
+        "usageNote"
+    ];
     return UserProfile;
 })();
 module.exports = UserProfile;
