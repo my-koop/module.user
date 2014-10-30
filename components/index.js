2b50d78b8d4c65b53cc848d3e47af47e440647a2
Mathieu M-Gosselin <mathieumg@gmail.com>
2014-10-30 19:15:08 -0400

    Now referring to the core components in the core module.

--- components/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  LoginBox:           require("./LoginBox.jsx"),
-  LoginPage:          require("./LoginPage.jsx"),
-  MyAccountPage:      require("./MyAccountPage.jsx"),
-  PasswordChangeForm: require("./PasswordChangeForm.jsx"),
-  ProfileUpdateForm:  require("./ProfileUpdateForm.jsx"),
-};
