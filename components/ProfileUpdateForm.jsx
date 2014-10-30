c5f57a3adeb9a523d28c6c20b933094731026381
ad <stepanian.adam@live.com>
2014-10-30 16:41:19 -0400

    Added a subset of register form fields to profile update form

--- components/ProfileUpdateForm.jsx
+++ components/ProfileUpdateForm.jsx
@@ -4,6 +4,9 @@ var UserProfile = require("../classes/UserProfile");
 var ajax = require("ajax");
 
 var ProfileUpdateForm = React.createClass({
+
+  mixins: [React.addons.LinkedStateMixin],
+
   propTypes: {
     profileData: React.PropTypes.object,
   },
@@ -46,7 +49,89 @@ var ProfileUpdateForm = React.createClass({
     return (
       <div>
         <form onSubmit={this.onSubmit}>
-           //FIX ME : Add static profile form
+           <BSInput
+                type="text"
+                label="Firstname"
+                placeholder="Firstname"
+                autoFocus
+                ref="firstname"
+                valueLink = {this.makeValueLink("firstname")}
+              />
+              <BSInput
+                type="text"
+                label="lastname"
+                placeholder="Last Name"
+                ref="lastname"
+                valueLink = {this.makeValueLink("lastname")}
+              />
+              <BSInput
+                type="email"
+                label="E-Mail"
+                placeholder="E-Mail"
+                ref="email"
+                valueLink = {this.makeValueLink("email")}
+              />
+              <BSInput
+                type="text"
+                label="Phone Number"
+                placeholder="Phone number"
+                ref="phone"
+                valueLink = {this.makeValueLink("phone")}
+              />
+              <BSInput
+                type="text"
+                label="Birthdate"
+                placeholder="Birthdate (YYYY/MM/DD)"
+                ref="Birthdate"
+                valueLink = {this.makeValueLink("birthdate")}
+              />
+              <BSInput
+                type="select"
+                defaultValue="visit"
+                label="How did you find us"
+                valueLink={this.linkState("referral")}
+              >
+                <option value="visit">On-Site Visit</option>
+                <option value="friend">Friend referral</option>
+                <option value="ads">Ads</option>
+                <option value="other">Other</option>
+              </BSInput>
+              {this.state.referral === "other" ?
+                <BSInput
+                  type="text"
+                  label="Please Specify"
+                  valueLink={this.makeValueLink("referralSpecify")}
+                />
+              : null
+              }
+              <BSInput
+                type="select"
+                defaultValue="everyday"
+                label="Bike usage"
+                valueLink = {this.makeValueLink("usageFrequency")}
+              >
+                <option value="everyday">Every Day</option>
+                <option value="fewWeek">Few times a week</option>
+                <option value="fewMonth">Few times a month</option>
+                <option value="fewYear">Few times a year</option>
+                <option value="never">Never</option>
+              </BSInput>
+              <BSInput
+                type="select"
+                defaultValue="udem"
+                label="Where do you come from?"
+                valueLink = {this.makeValueLink("origin")}
+              >
+                <option value="udem">Université de Montréal</option>
+                <option value="brebeuf">College Jean-De-Brébeuf</option>
+                <option value="other">Other</option>
+              </BSInput>
+              <BSInput
+                type="text"
+                label="Why do you use a bike"
+                placeholder="Describe why you mainly use your bike"
+                valueLink = {this.makeValueLink("usageNote")}
+              />
           <BSInput type="submit" bsStyle="primary" value="Update Profile" />
         </form>
       </div>
