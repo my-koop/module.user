6d2b67c3ceda1ef80ca00edcafc1a114f5cdef73
ad <stepanian.adam@live.com>
2014-11-04 21:43:16 -0500

    Forgot to add controller

---
+++ controllers/updateProfile.ts
@@ -0,0 +1,34 @@
+import express = require("express");
+import getLogger = require("mykoop-logger");
+var logger = getLogger(module);
+
+function updateProfile(req: express.Request, res: express.Response) {
+  var id = parseInt(req.param("id", -1));
+  if(id === -1) {
+    return res.send(400);
+  }
+  var newProfile: mkuser.UserProfile = {
+    email          : req.param("email"),
+    firstname      : req.param("firstname"),
+    lastname       : req.param("lastname"),
+    birthdate      : new Date(req.param("birthdate")),
+    phone          : req.param("phone"),
+    origin         : req.param("origin"),
+    referral       : req.param("referral"),
+    usageFrequency : req.param("usageFrequency"),
+    usageNote      : req.param("usageNote")
+  };
+
+  this.updateProfile(id,newProfile, function(err, success) {
+    if (err) {
+      logger.verbose(err.toString());
+      return res.status(500).send(err.toString());
+    }
+
+    res.send({
+      updateSuccess: success
+    });
+  });
+};
+
+export = updateProfile;
