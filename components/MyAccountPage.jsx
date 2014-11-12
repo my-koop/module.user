547b346bc08210825ccf5318142d75bb3a1f4529
ad <stepanian.adam@live.com>
2014-11-12 02:51:10 -0500

    Getting additional components from MetaData

--- components/MyAccountPage.jsx
+++ components/MyAccountPage.jsx
@@ -1,29 +1,46 @@
-﻿var React                = require("react");
+var React                = require("react");
 var PropTypes            = React.PropTypes;
 var BSCol                = require("react-bootstrap/Col");
 var BSTabbedArea         = require("react-bootstrap/TabbedArea");
 var BSTabPane            = require("react-bootstrap/TabPane");
 var MKPasswordChangeForm = require("./PasswordChangeForm");
 var MKProfileUpdateForm  = require("./ProfileUpdateForm");
-var __                   = require("language").__;
+
+
+var __ = require("language").__;
+var _  = require("lodash");
+
+var myAccountPlugins = require("dynamic-metadata").myAccountPlugins;
 
 var MyAccountPage = React.createClass({
 
   render: function() {
+    var tabsInfo = [
+      {
+        component: function() { return MKProfileUpdateForm; },
+        titleKey: "user::myaccount_tab_profile"
+      },
+      {
+        component: function() { return MKPasswordChangeForm; },
+        titleKey: "user::myaccount_tab_password"
+      }
+    ].concat(_.toArray(myAccountPlugins));
+
+    var additionalTabs = _.map(tabsInfo, function(plugin, index) {
+      var PluginComponent = plugin.component();
+      return (
+        <BSTabPane key={index} tab={__(plugin.titleKey)}>
+          <BSCol md={4} sm={6}>
+            <PluginComponent id={2} />
+          </BSCol>
+        </BSTabPane>
+      );
+    });
+
     return (
       <BSCol>
-        <BSTabbedArea defaultActiveKey={1}>
-          <BSTabPane key={1} tab={__("user::myaccount_tab_profile")}>
-            <BSCol md={4} sm={6}>
-              <MKProfileUpdateForm />
-            </BSCol>
-          </BSTabPane>
-
-          <BSTabPane key={2} tab={__("user::myaccount_tab_password")}>
-            <BSCol md={4} sm={6}>
-              <MKPasswordChangeForm />
-            </BSCol>
-          </BSTabPane>
+        <BSTabbedArea defaultActiveKey={0}>
+          {additionalTabs}
         </BSTabbedArea>
       </BSCol>
     );
