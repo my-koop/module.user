670693b68d92b0578267c3300e52ee3a989d4885
Mathieu M-Gosselin <mathieumg@gmail.com>
2014-11-26 21:24:26 -0500

    Changes following CR.

--- components/AdminEditProfile.jsx
+++ components/AdminEditProfile.jsx
@@ -3,28 +3,22 @@ var React = require("react");
 var BSCol = require("react-bootstrap/Col");
 
 var MKUserProfileWithTabs = require("./UserProfileWithTabs");
-var MKPermissionMixin = require("./PermissionMixin");
-var MKPermissionRedirectMixin = require("./PermissionRedirectMixin");
 
 var __ = require("language").__;
 
 var AdminEditProfile = React.createClass({
-  mixins: [MKPermissionMixin, MKPermissionRedirectMixin],
-
   render: function() {
     var userId = parseInt(this.props.params.id);
     return (
-      this.state.userMeetsPermissions ?
-        <BSCol>
-          <h1>
-            {__("user::adminEditWelcome", {userId: userId})}
-          </h1>
-          <MKUserProfileWithTabs
-            userId={userId}
-            metaPlugins="adminEditPlugins"
-          />
-        </BSCol>
-      : null
+      <BSCol>
+        <h1>
+          {__("user::adminEditWelcome", {userId: userId})}
+        </h1>
+        <MKUserProfileWithTabs
+          userId={userId}
+          metaPlugins="adminEditPlugins"
+        />
+      </BSCol>
     );
   }
 });
